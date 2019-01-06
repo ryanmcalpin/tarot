39a9e52420147af6908df6c2b5899b9f19581431
Ryan McAlpin <rhmcalpin@gmail.com>
2019-01-05 16:46:43 -0800

    complete getDeck method in Deck.java

--- app/src/main/java/com/example/rygn/tarot/Deck.java
+++ app/src/main/java/com/example/rygn/tarot/Deck.java
@@ -5,10 +5,59 @@ import java.util.List;
 
 public class Deck {
     List<String> cards = new ArrayList<>();
+    List<String> minorArcNoSuit = new ArrayList<>();
+    List<String> suits = new ArrayList<>();
 
     public List<String> getDeck() {
-        cards.add("c1");
-        cards.add("c2");
+        cards.add("The Magician");
+        cards.add("The High Priestess");
+        cards.add("The Empress");
+        cards.add("The Emperor");
+        cards.add("The Hierophant");
+        cards.add("The Lovers");
+        cards.add("The Chariot");
+        cards.add("Strength");
+        cards.add("The Hermit");
+        cards.add("Wheel of Fortune");
+        cards.add("Justice");
+        cards.add("The Hanged Man");
+        cards.add("Death");
+        cards.add("Temperance");
+        cards.add("The Devil");
+        cards.add("The Tower");
+        cards.add("The Star");
+        cards.add("The Moon");
+        cards.add("The Sun");
+        cards.add("Judgement");
+        cards.add("The World");
+        cards.add("The Fool");
+
+        minorArcNoSuit.add("Ace");
+        minorArcNoSuit.add("Two");
+        minorArcNoSuit.add("Three");
+        minorArcNoSuit.add("Four");
+        minorArcNoSuit.add("Five");
+        minorArcNoSuit.add("Six");
+        minorArcNoSuit.add("Seven");
+        minorArcNoSuit.add("Eight");
+        minorArcNoSuit.add("Nine");
+        minorArcNoSuit.add("Ten");
+        minorArcNoSuit.add("Page");
+        minorArcNoSuit.add("Knight");
+        minorArcNoSuit.add("Queen");
+        minorArcNoSuit.add("King");
+
+        suits.add("Swords");
+        suits.add("Wands");
+        suits.add("Pentacles");
+        suits.add("Cups");
+
+        for (int i = 0; i < suits.size(); i++) {
+            for (int ii = 0; ii < minorArcNoSuit.size(); ii++) {
+                cards.add(minorArcNoSuit.get(ii) + " of " + suits.get(i));
+            }
+        }
+
         return cards;
     }
 }
