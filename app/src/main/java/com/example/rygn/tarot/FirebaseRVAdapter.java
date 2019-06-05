d1015dfa918e9a6fd224faf5493c2a2039fbdc4d
Ryan McAlpin <rhmcalpin@gmail.com>
2019-06-04 23:16:10 -0700

    begin work on adding recyclerview

---
+++ app/src/main/java/com/example/rygn/tarot/FirebaseRVAdapter.java
@@ -0,0 +1,57 @@
+package com.example.rygn.tarot;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FirebaseRVAdapter extends RecyclerView.Adapter<FirebaseRVAdapter.FirebaseViewHolder> {
+    private ArrayList<String> mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class FirebaseViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textView;
+        public FirebaseViewHolder(TextView v) {
+            super(v);
+            textView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public FirebaseRVAdapter(ArrayList<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public FirebaseRVAdapter.FirebaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.spread_list_item, parent, false);
+
+        FirebaseViewHolder vh = new FirebaseViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(FirebaseViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.setText(mDataset.get(position));
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+
+}
